9af22606647bd956321b085509e2272cc26532d0
Your Name <you@example.com>
2020-09-14 11:16:53 +0900

    t

--- dreamhack/rev-basic-7/gen.py
+++ dreamhack/rev-basic-7/gen.py
@@ -1,11 +1,27 @@
 serial = """52 DF B3 60 F1 8B 1C B5 57 D1 9F 38 4B 29 D9 26 7F C9 A3 E9 53 18 4F B8 6A CB 87 58 5B 39 1E""".split(' ')
 
-serial = [ int(x) for x in serial]
+def rotr(num, bits):
+    bits = bits % 8
+    for x in range(bits):
+        bit = num & 1
+        num >>= 1
+        if(bit):
+            num |= (1 << 7)
+    return num
 
+print rotr(10, 11)
+serial = [ int(x, 16) for x in serial]
+flag = ''
 for idx in range(0x1f):
     tp = serial[idx] ^ idx 
     if idx % 7 == 0:
-        tp ^ 
+        tp = rotr(tp, idx)
+    flag += chr(tp)
+print flag
+
+    
+    
+
 
 
 
